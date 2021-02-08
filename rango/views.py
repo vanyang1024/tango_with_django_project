c1724d055fa6c3e627fc52310175fbeb6bc3ae71
Shimeng Yang <2604831Y@student.gla.ac.uk>
2021-02-08 00:24:27 +0000

    chatper7

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rango.models import Category, Page
 from django.http import HttpResponse 
+from rango.forms import CategoryForm, PageForm
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 def index(request):
@@ -51,7 +54,52 @@ def show_category(request, category_name_slug):
 	return render(request, 'rango/category.html', context=context_dict)
 
 
+def add_category(request):
+
+	form = CategoryForm()
+# A HTTP POST?
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		print(form.is_valid())
+		# Have we been provided with a valid form?
+		if form.is_valid():
+# Save the new category to the database. 
+			form.save(commit=True)
+# Now that the category is saved, we could confirm this. # For now, just redirect the user back to the index view. 
+			return redirect('/rango/')
+		else:
+# The supplied form contained errors -
+# just print them to the terminal.
+			print(form.errors)
+			response = HttpResponse('Category with this Name already exists.')
+			return response
+# Will handle the bad form, new form, or no form supplied cases. # Render the form with error messages (if any).
+	return render(request, 'rango/add_category.html', {'form': form})
 
 
+def add_page(request, category_name_slug): 
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+    # You cannot add a page to a Category that does not exist...
+	if category is None:
+		return redirect('/rango/')
+	
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid(): 
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.save()
+				
+				return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+		else: 
+			print(form.errors)
+	
+	context_dict = {'form': form, 'category': category}
+	return render(request, 'rango/add_page.html', context=context_dict)
 
 
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -26,6 +26,4 @@ urlpatterns = [
 	path('rango/', include('rango.urls')),
 	# The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
-    path('category/<slug:category_name_slug>/',
-         views.show_category, name='show_category'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
