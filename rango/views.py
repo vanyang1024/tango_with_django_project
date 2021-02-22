5b066ff8b1a96cf8b24f92500624f901eb39eb65
Shimeng Yang <2604831Y@student.gla.ac.uk>
2021-02-22 01:28:07 +0000

    chatper10

--- rango/views.py
+++ rango/views.py
@@ -27,24 +27,23 @@ def get_server_side_cookie(request, cookie, default_val=None):
 		val = default_val 
 	return val
 
-def visitor_cookie_handler(request, response):
-# Get the number of visits to the site.
-# We use the COOKIES.get() function to obtain the visits cookie.
-# If the cookie exists, the value returned is casted to an integer. 
-# If the cookie doesn't exist, then the default value of 1 is used. 
-	visits = int(request.COOKIES.get('visits', '1'))
-	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-	last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1')) 
+	last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+'%Y-%m-%d %H:%M:%S')
     # If it's been more than a day since the last visit...
 	if (datetime.now() - last_visit_time).days > 0:
 		visits = visits + 1
-# Update the last visit cookie now that we have updated the count 
-		response.set_cookie('last_visit', str(datetime.now()))
+# Update the last visit cookie now that we have updated the count request.session['last_visit'] = str(datetime.now())
 	else:
 # Set the last visit cookie 
-		response.set_cookie('last_visit', last_visit_cookie)
+		request.session['last_visit'] = last_visit_cookie
     # Update/set the visits cookie
-	response.set_cookie('visits', visits)
+	request.session['visits'] = visits
+
 
 
 def index(request):
@@ -59,6 +58,8 @@ def index(request):
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
 	
+	visitor_cookie_handler(request)
+
     # Render the response and send it back!
 	
 	response = render(request, 'rango/index.html', context=context_dict)
@@ -72,7 +73,7 @@ def about(request):
 	context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
 
 	response =  render(request, 'rango/about.html',context_dict)
-	visitor_cookie_handler(request, response)
+	visitor_cookie_handler(request)
 	return response
 
 
